c978428ea56472d3eab67bb9a8ae37c9c2c3d902
canvas-j <1341281680@qq.com>
2018-09-03 13:50:02 +0800

    修改

--- join_file/join_csv.py
+++ join_file/join_csv.py
@@ -4,7 +4,8 @@ import pandas as pd
 import glob
 
 class MerageTool:
-    def merage_all():
+
+    def merage_csv():
         csv_list = glob.glob('file/*.csv')
         print(u'共发现{}个CSV文件'.format(len(csv_list)))
         print(u'正在处理............')
@@ -18,5 +19,20 @@ class MerageTool:
         datalist.to_csv("result.csv", encoding="gb18030")
         print(u'>>>>>>去重完毕！<<<<<<')
 
+    def merage_excel():
+        excel_list = glob.glob('file/*.xlsx')
+        print(u'共发现{}个excel文件'.format(len(excel_list)))
+        print(u'正在处理............')
+        frames = []
+        for c_name in excel_list:
+            frames.append(pd.read_excel(c_name, encoding="gb18030"))
+        result = pd.concat(frames)
+        result = result.reset_index(drop=True)
+        print(u'>>>>>>合并完毕！<<<<<<')
+        datalist = result.drop_duplicates(subset=['bu_links', 'c_address', 'c_name', 'c_product', 'co_links'], keep='first', inplace=False)
+        datalist.to_csv("result.xlsx", encoding="gb18030")
+        print(u'>>>>>>去重完毕！<<<<<<')
+
 if __name__ == '__main__':
-    MerageTool.merage_all()
+    MerageTool.merage_csv()
+    # MerageTool.merage_excel()
