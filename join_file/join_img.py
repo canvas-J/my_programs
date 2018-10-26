406f0d1d2e01d0e02d16cd6803bc56ad0c9996ed
canvas-j <ja.ga.main@qq.com>
2018-10-26 22:17:28 +0800

    添加拼接长图模块，好用不挑人

--- join_file/join_img.py
+++ join_file/join_img.py
@@ -1,5 +1,21 @@
+# -*- encoding=utf-8 -*-
 import os, datetime, random
 from PIL import Image
+import numpy as np
+
+def at_bottom(img_paths):
+    base_img = Image.open(img_paths[0])
+    base_sz = base_img.size
+    base_mat = np.atleast_2d(base_img)
+    for img_p in img_paths[1:]:
+        im = Image.open(img_p) #resize to same width
+        new_sz = im.size
+        if new_sz != base_sz:
+            im = im.resize((base_sz[0], round(new_sz[0] / base_sz[0] * new_sz[1])), Image.ANTIALIAS)
+        mat = np.atleast_2d(im)
+        base_mat = np.append(base_mat, mat, axis=0)
+    long_img = Image.fromarray(base_mat)
+    long_img.save('{}long.png'.format(now))
 
 def add_imgPath(dirName=os.getcwd()):
     img_paths = []
@@ -26,14 +42,16 @@ def join_img(img_paths):
             if num >= len(img_paths):
                 num = 0
     print(toImage.size)
-    now = datetime.datetime.now().strftime('%m-%d-%M')
     toImage.save('{}merged.png'.format(now))
 
+
 if __name__ == '__main__':
+    now = datetime.datetime.now().strftime('%m-%d-%M')
+    dirName = 'D:/Wrapbuffer/bing壁纸' # 素材图片位置
     img_w = 192 # 缩略图尺寸
     img_h = 108
     line_max = 20 # 大图纵横数量
     row_max = 20
-    dirName = 'D:/Wrapbuffer/bing壁纸' # 素材图片位置
     img_paths = add_imgPath(dirName)
     join_img(img_paths)
+    # at_bottom(img_paths)
