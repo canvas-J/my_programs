8a45b88d72b4d5bd6f1c1c9104cfa50c57a726c7
canvas-j <ja.ga.main@qq.com>
2018-12-11 21:10:31 +0800

    理解并优化参数，识别80%黑头

--- detect_blob.py
+++ detect_blob.py
@@ -5,26 +5,33 @@ import numpy as np
 
 # 读取图像
 # im = cv2.imread("photos/0.jpg", cv2.IMREAD_GRAYSCALE)
-im = cv2.imread("photos/2.jpg")
+im = cv2.imread("photos/1.jpg")
 im = im[2776:3708, 288:1624] # y0,y1,x0,x1
 # 设置简单的斑点监测参数
 params = cv2.SimpleBlobDetector_Params()
+# 斑点距离、重复情况
+params.minRepeatability = 2
+params.minDistBetweenBlobs = 8
 # 设置阈值大小
+# params.thresholdStep = 10
 params.minThreshold = 5
-params.maxThreshold = 150
-# 按面积过滤
+params.maxThreshold = 600
+# 按颜色过滤
+params.filterByColor = True
+params.blobColor = 0
+# 按面积过滤（基于像素单位的面积）
 params.filterByArea = True
-params.minArea = 10
-# 按圆度过滤
+params.minArea = 9
+params.maxArea = 600
+# 按圆度过滤（0.75-0.8为矩形正多边形），越大越圆
 params.filterByCircularity = True
-params.minCircularity = 0.1
-# 按凸度过滤
-params.filterByConvexity = True
-params.minConvexity = 0.87
-
-# 按惯性过滤
+params.minCircularity = 0.2
+# 按惯性过滤，越大越圆
 params.filterByInertia = True
-params.minInertiaRatio = 0.01
+params.minInertiaRatio = 0.36
+# 按凸度过滤，越大越圆
+params.filterByConvexity = True
+params.minConvexity = 0.3
 # 用以上参数创建滤波器
 ver = (cv2.__version__).split('.')
 if int(ver[0]) < 3 :
